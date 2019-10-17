1744f29e3bfca274d7fd36747c35d0987a623120
Victor Tran <vicr12345@gmail.com>
2019-10-17 23:13:26 +1100

    Initial Commit

---
+++ entertaining.cpp
@@ -0,0 +1,66 @@
+/****************************************
+ *
+ *   INSERT-PROJECT-NAME-HERE - INSERT-GENERIC-NAME-HERE
+ *   Copyright (C) 2019 Victor Tran
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *************************************/
+#include "entertaining.h"
+
+#include <QIcon>
+
+QIcon Entertaining::iconForButton(QGamepadManager::GamepadButton button)
+{
+    switch (button) {
+        case QGamepadManager::ButtonA:
+            break;
+        case QGamepadManager::ButtonB:
+            break;
+        case QGamepadManager::ButtonX:
+            break;
+        case QGamepadManager::ButtonY:
+            break;
+        case QGamepadManager::ButtonL1:
+            break;
+        case QGamepadManager::ButtonR1:
+            break;
+        case QGamepadManager::ButtonL2:
+            break;
+        case QGamepadManager::ButtonR2:
+            break;
+        case QGamepadManager::ButtonSelect:
+            break;
+        case QGamepadManager::ButtonStart:
+            break;
+        case QGamepadManager::ButtonL3:
+            break;
+        case QGamepadManager::ButtonR3:
+            break;
+        case QGamepadManager::ButtonUp:
+            break;
+        case QGamepadManager::ButtonDown:
+            break;
+        case QGamepadManager::ButtonRight:
+            break;
+        case QGamepadManager::ButtonLeft:
+            break;
+        case QGamepadManager::ButtonCenter:
+            break;
+        case QGamepadManager::ButtonGuide:
+            break;
+        default:
+            break;
+    }
+}
---
+++ libentertaining.pro
@@ -0,0 +1,60 @@
+QT += widgets gamepad thelib
+
+TEMPLATE = lib
+DEFINES += LIBENTERTAINING_LIBRARY
+TARGET += entertaining
+
+CONFIG += c++11
+
+# The following define makes your compiler emit warnings if you use
+# any Qt feature that has been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    entertaining.cpp
+
+HEADERS += \
+    libentertaining_global.h \
+    entertaining.h
+
+DISTFILES += \
+    qt_libentertaining.pri
+
+# Install rules
+header.files = *.h
+module.path = $$[QMAKE_MKSPECS]/modules
+
+unix {
+    module.files = qt_libentertaining.pri
+}
+
+unix:!macx {
+    target.path = /usr/lib
+    header.path = /usr/include/libentertaining
+    module.files = qt_libentertaining.pri
+}
+
+macx {
+    CONFIG(debug, debug|release): TARGET = libentertaining_debug
+
+    target.path = /usr/local/lib
+    header.path = /usr/local/include/libentertaining
+    module.files = qt_libentertaining.pri
+}
+
+win32 {
+    CONFIG(debug, debug|release): TARGET = libentertainingd
+
+    module.files = qt_thelib.pri
+    header.path = "C:/Program Files/thelibs/include"
+    target.path = "C:/Program Files/thelibs/lib"
+}
+
+INSTALLS += target module header
